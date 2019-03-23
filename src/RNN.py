d039683c775f42ac39cfd100d2fff3f54230a7fa
Shyama <shyamap95@gmail.com>
2019-03-23 20:17:55 +0000

    Updated back propagation

--- src/RNN.py
+++ src/RNN.py
@@ -12,6 +12,8 @@ class RNN:
     output = 0
     V = 153
     H = 64
+    D = 100
+    h_all = []
 
     def __init__(self, h, v):
         self.H = h
@@ -25,6 +27,7 @@ class RNN:
 
     def forward(self, input):
         self.h = np.zeros(self.H, dtype=float)
+        self.h_all.append(self.h)
 
         for inp in input:
             x = self.input_layer(inp)
@@ -33,15 +36,48 @@ class RNN:
         self.compute_output()
         return self.output
 
-    def backward(self):
-        ...
-        # unrolls the RNN and performs back propagation through time, computes and updates the state variables
-        self.Whh = []
-        self.Wxh = []
-        self.Why = []
+    def backward(self, input, target):
+        N = 10
+        eta = 0.001
+        initial_loss = self.total_loss(input, target)
+        for epoch in range(N):
+            j = 0
+            print('Epoch : ' + str(epoch + 1))
+            print('Initial loss = ' + str(initial_loss))
+
+            Wxh = self.Wxh.copy()
+            Whh = self.Whh.copy()
+            Why = self.Why.copy()
+            bias = self.bias
+
+            for value in input:
+                input_values = value.split(' ')
+                input_values.remove('')
+                input_values = list(map(int, input_values))
+                [dL_dWxh, dL_dWhh, dL_dWhy, dL_dbias] = self.gradient_computation(input_values, target[j])
+                self.Wxh = self.Wxh + eta * dL_dWxh
+                self.Whh = self.Whh + eta * dL_dWhh
+                self.Why = self.Why + eta * dL_dWhy
+                self.bias = self.bias + eta * dL_dbias
+                j += 1
+
+            new_loss = self.total_loss(input, target)
+
+            if new_loss > initial_loss:
+                self.Wxh = Wxh
+                self.Whh = Whh
+                self.Why = Why
+                self.bias = bias
+                print('Loss set to : ' + str(self.total_loss(input, target)))
+                break
+            else:
+                initial_loss = new_loss
+
+        return [self.Wxh, self.Whh, self.Why, self.bias]
 
     def next_state(self, x):
         self.h = np.tanh(np.dot(self.Whh, self.h) + np.dot(self.Wxh, x))
+        self.h_all.append(self.h)
 
     def compute_output(self):
         y = np.dot(self.Why, self.h) + self.bias
@@ -55,3 +91,42 @@ class RNN:
 
     def sigmoid(self, x):
         return 1/(1+np.exp(-x))
+
+    def cross_entropy_loss(self, target, predicted):
+        loss = target * np.log(predicted + 1e-10) + (1 - target) * np.log(1 - predicted + 1e-10)
+        return loss
+
+    def total_loss(self, input, target):
+        total_loss = 0
+        i = 0
+        for value in input:
+            input_values = value.split(' ')
+            input_values.remove('')
+            predicted = self.forward(input_values)
+            total_loss -= self.cross_entropy_loss(target[i], predicted)
+            i += 1
+        total_loss = total_loss / len(input)
+        return total_loss
+
+    def gradient_computation(self, input, target):
+        predicted = self.forward(input)
+        dL_dWhy = np.transpose((target - predicted) * self.h)
+        dL_dbias = target - predicted
+
+        dL_dWxh = np.zeros(self.Wxh.shape)
+        dL_dWhh = np.zeros(self.Whh.shape)
+
+        input_length = len(input)
+        h_list = self.h_all.copy()
+        delta_t = (target - predicted) * (np.eye(self.H) -
+                                          np.matmul(h_list[input_length], np.transpose(h_list[input_length])))
+
+        for t in range(input_length-1, input_length - self.D, -1):
+            xt = self.input_layer(input[t - 1]).reshape((1, self.V))
+            dL_dWxh += np.matmul(delta_t, np.transpose(self.Why)) * xt
+            dL_dWhh += delta_t * h_list[t]
+            delta_t = delta_t * self.Whh * (np.eye(self.H) - np.matmul(h_list[t], np.transpose(h_list[t])))
+
+        dL_dWhh = dL_dWhh * self.Why
+
+        return [dL_dWxh, dL_dWhh, dL_dWhy, dL_dbias]
--- trainModel.py
+++ trainModel.py
@@ -1,4 +1,6 @@
 import numpy as np
+import time
+
 from src.Model import *
 from src.RNN import *
 
@@ -19,10 +21,32 @@ with open(target, 'r') as file:
 targetData = targetData.splitlines(keepends=False)
 targetData = list(map(int, targetData))
 
+testData = ''
+with open('testData/test_data.txt', 'r') as file:
+    testData = file.read()
+testData = testData.splitlines(keepends=False)
+
 V = model.V
 
-for value in trainingData:
+# for value in trainingData:
+#     input_values = value.split(' ')
+#     input_values.remove('')
+#     output = rnn.forward(input_values)
+#     print(output)
+
+# print(rnn.total_loss(trainingData, targetData))
+start = time.time()
+rnn.backward(trainingData, targetData)
+start = time.time() - start
+print('Total time taken = ' + str(start))
+
+output_csv = ''
+for value in testData:
     input_values = value.split(' ')
     input_values.remove('')
     output = rnn.forward(input_values)
-    print(output)
+    output_csv += str(output) + ','
+
+print(output_csv)
+with open('bestModel/output.csv', 'w') as file:
+    file.write(output_csv)
